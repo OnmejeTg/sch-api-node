65bfbda82bab8d9bce021cb2cf15dba29f77210c
Onmeje TG <tonmeje@gmail.com>
2024-05-12 09:24:13 +0100

    academic year refcator

--- routes/academicYear.js
+++ routes/academicYear.js
@@ -13,7 +13,7 @@ const academicYearRouter = express.Router();
 academicYearRouter.post("/create", isAdmin, createAcademicYear);
 academicYearRouter.get("/", isAdmin, getAcademicYears);
 academicYearRouter.get("/:id", isAdmin, getAcademicYearById);
-academicYearRouter.put("update/:id", isAdmin, updateAcademicYear);
-academicYearRouter.delete("delete/:id", isAdmin, deleteAcademicYear);
+academicYearRouter.put("/update/:id", isAdmin, updateAcademicYear);
+academicYearRouter.delete("/delete/:id", isAdmin, deleteAcademicYear);
 
 export default academicYearRouter;
