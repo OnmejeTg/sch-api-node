86c31b47df18e3e96e5b852f4d94f6254d9332ce
Onmeje TG <tonmeje@gmail.com>
2024-05-15 12:52:29 +0100

    exam crud

--- controllers/teachers/examCtrl.js
+++ controllers/teachers/examCtrl.js
@@ -6,44 +6,56 @@ const createExam = asyncHandler(async (req, res) => {
   const {
     name,
     description,
-    subjectId,
-    academicTermId,
-    programID,
+    subject,
+    academicTerm,
+    program,
     examDate,
     examTime,
     classLevel,
-    academicYearID,
+    academicYear,
   } = req.body;
 
-  //Get teacher id
-  const teacherFound = await Teacher.findById(req.user.id);
+  // Get teacher id
+  const teacher = await Teacher.findById(req.user.id);
 
-  //check if exsma is already exists
-  const examExists = await Exam.findOne({
+  // Check if exam already exists
+  const examExists = await Exam.exists({
     name,
-    subject: subjectId,
-    academicTerm: academicTermId,
-    academicYear: academicYearID,
-    program: programID,
-    classLevel: classLevel,
+    subject,
+    academicTerm,
+    academicYear,
+    program,
+    classLevel,
     examDate,
     examTime,
   });
+
   if (examExists) {
-    res.status(400);
     throw new Error("Exam already exists");
   }
 
+  // Create new exam
   const newExam = new Exam({
     name,
     description,
-    subject: subjectId,
-    teacher: teacherFound._id,
-    academicTerm: academicTermId,
+    subject,
+    teacher: teacher._id,
+    academicTerm,
+    academicYear,
+    program,
+    classLevel,
+    examDate,
+    examTime,
+    createdBy: teacher._id, // Assuming req.user.id is available
   });
-  teacherFound.examsCreated.push(newExam);
-  await teacherFound.save();
+
+  // Save new exam
   const exam = await newExam.save();
+
+  // Update teacher's examsCreated
+  teacher.examsCreated.push(newExam._id);
+  await teacher.save();
+
   res.status(200).json({
     success: true,
     message: "Exam created successfully",
@@ -51,4 +63,70 @@ const createExam = asyncHandler(async (req, res) => {
   });
 });
 
-export { createExam };
+// READ Exam
+const getExam = asyncHandler(async (req, res) => {
+  const examId = req.params.id;
+
+  const exam = await Exam.findById(examId); // Assuming you want to populate teacher details
+
+  if (!exam) {
+    res.status(404);
+    throw new Error("Exam not found");
+  }
+
+  res.status(200).json({
+    success: true,
+    data: exam,
+  });
+});
+
+// UPDATE Exam
+const updateExam = asyncHandler(async (req, res) => {
+  const examId = req.params.id;
+  const updateData = req.body;
+  const updatedExam = await Exam.findByIdAndUpdate(examId, updateData, {
+    new: true,
+  });
+
+  if (!updatedExam) {
+    res.status(404).json({
+      success: false,
+      message: "Exam not found",
+    });
+  } else {
+    res.status(200).json({
+      success: true,
+      message: "Exam updated successfully",
+      data: updatedExam,
+    });
+  }
+});
+
+// DELETE Exam
+const deleteExam = asyncHandler(async (req, res) => {
+  const examId = req.params.id;
+  const deletedExam = await Exam.findByIdAndDelete(examId);
+  if (!deletedExam) {
+    res.status(404).json({
+      success: false,
+      message: "Exam not found",
+    });
+  } else {
+    res.status(200).json({
+      success: true,
+      message: "Exam deleted successfully",
+    });
+  }
+});
+
+// READ All Exams
+const getAllExams = asyncHandler(async (req, res) => {
+  const exams = await Exam.find(); // Assuming you want to populate teacher details
+
+  res.status(200).json({
+    success: true,
+    data: exams,
+  });
+});
+
+export { createExam, getExam, updateExam, deleteExam, getAllExams };
--- routes/examRouter.js
+++ routes/examRouter.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { createExam } from '../controllers/teachers/examCtrl.js';
+import { createExam, deleteExam, getAllExams, getExam, updateExam } from '../controllers/teachers/examCtrl.js';
 import { isTeacher } from '../middleware/auth.js';
 
 const examRouter = express.Router();
 
 
 examRouter.post('/create', isTeacher, createExam)
+examRouter.get('/all-exams', isTeacher, getAllExams)
+examRouter.get('/:id', isTeacher, getExam)
+examRouter.put('/update/:id', isTeacher, updateExam)
+examRouter.delete('/delete/:id', isTeacher, deleteExam)
 
 export default examRouter
