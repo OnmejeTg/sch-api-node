63ab31f1c598433906450770351fc74f898a285e
Onmeje TG <tonmeje@gmail.com>
2024-05-28 09:26:56 +0100

    added credentials

--- app/app.js
+++ app/app.js
@@ -27,6 +27,7 @@ const corsOptions = {
   origin: ['https://new-project-35520.web.app', 'https://new-project-35520.web.app', 'http://localhost:5173'],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   allowedHeaders: 'Authorization,Origin,X-Requested-With,Content-Type,Accept',
+  credentials: true // Allow credentials
 
 };
 
