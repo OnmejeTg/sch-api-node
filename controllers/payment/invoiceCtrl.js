fac5c1b862ce30ca07603d7efd45ec599c1fc2f8
Onmeje TG <tonmeje@gmail.com>
2024-06-02 19:33:47 +0100

    refactored invoice

--- controllers/payment/invoiceCtrl.js
+++ controllers/payment/invoiceCtrl.js
@@ -6,7 +6,11 @@ const createFee = asyncHandler(async (req, res) => {
     const { name, amount, amountInWords, description } = req.body;
     const newFee = new Fee({ name, amount, amountInWords, description });
     const savedFee = await newFee.save();
-    res.status(201).json(savedFee);
+    res.status(201).json({
+      status: "success",
+      message: "Fee saved successfully",
+      data: savedFee,
+    });
   } catch (error) {
     res.status(500).json({ message: "Error creating fee", error });
   }
@@ -15,8 +19,18 @@ const createFee = asyncHandler(async (req, res) => {
 const getFees = asyncHandler(async (req, res) => {
   try {
     const fees = await Fee.find();
+
+    if (fees.length === 0) {
+      return res.status(204).json({
+        status: "success",
+        message: "Fees not found",
+        data: null,
+      });
+    }
+
     res.status(200).json({
       status: "success",
+      message: "Fees found",
       data: fees,
     });
   } catch (error) {
@@ -28,8 +42,24 @@ const getFee = asyncHandler(async (req, res) => {
   try {
     const { id } = req.params;
     const fee = await Fee.findById(id);
-    res.status(200).json(fee);
+
+    if (!fee) {
+      return res.status(204).json({
+        status: "success",
+        message: "Fee not found",
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Fee found",
+      data: fee,
+    });
   } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(400).json({ message: "Invalid fee ID format" });
+    }
     res.status(500).json({ message: "Error fetching fee", error });
   }
 });
@@ -43,7 +73,11 @@ const updateFee = asyncHandler(async (req, res) => {
       { new: true, runValidators: true }
     );
     if (updatedFee) {
-      res.status(200).json(updatedFee);
+      res.status(200).json({
+        status: "success",
+        message: "Fee was successfully updated",
+        data: updatedFee,
+      });
     } else {
       res.status(404).json({ message: "Fee not found" });
     }
