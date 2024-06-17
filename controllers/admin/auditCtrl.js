96fb593ac702986ed9afa8631ae7aee2f52febb6
Onmeje TG <tonmeje@gmail.com>
2024-06-17 19:30:58 +0100

    crud for audit

--- controllers/admin/auditCtrl.js
+++ controllers/admin/auditCtrl.js
@@ -21,10 +21,40 @@ const getAudit = asyncHandler(async (req, res) => {
     })
 })
 
+const getSingleAudit = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const audit = await Audit.findById(id);
+    if (!audit) {
+        res.status(404).json({ message: "Audit not found" });
+    } else {
+        res.status(200).json({
+            status: "success",
+            message: "Audit was successfully found",
+            data: audit,
+        });
+    }
+})
+
+const deleteAudit = asyncHandler(async(req, res)=>{
+    const { id } = req.params;
+    const deletedAudit = await Audit.findByIdAndDelete(id);
+    if (!deletedAudit) {
+        res.status(404).json({ message: "Audit not found" });
+    } else {
+        res.status(200).json({
+            status: "success",
+            message: "Audit was successfully deleted",
+            data: deletedAudit,
+        });
+    }
+})
+
 
 
 
 export {
     createAudit,
-    getAudit
+    getAudit,
+    getSingleAudit,
+    deleteAudit
 }
--- routes/auditRouter.js
+++ routes/auditRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createAudit, getAudit } from "../controllers/admin/auditCtrl.js";
+import { createAudit, getAudit, getSingleAudit, deleteAudit } from "../controllers/admin/auditCtrl.js";
 
 const auditRouter = express.Router();
 
 auditRouter.post('/create', createAudit);
-auditRouter.get('/audits', getAudit)
+auditRouter.get('/all-audits', getAudit)
+auditRouter.get('/:id', getSingleAudit)
+auditRouter.delete('/:id', deleteAudit)
 
 
 
