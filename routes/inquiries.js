6793c806135c12f46c8377e9cf2ddd80e03f83df
Onmeje TG <tonmeje@gmail.com>
2024-06-29 18:12:44 +0100

    added delete inquiry

--- routes/inquiries.js
+++ routes/inquiries.js
@@ -13,7 +13,8 @@ const inquiresRouter = express.Router();
 inquiresRouter.post("/", isLoggedin, isStudent, createInquiry);
 inquiresRouter.get("/", isLoggedin, isAdmin, getInquires);
 inquiresRouter.get("/:id", isLoggedin, isAdmin, getSingleInquiry);
+
 // inquiresRouter.put("/:id", isLoggedin, isAdmin, updateInquiry);
-// inquiresRouter.delete("/:id",isLoggedin, isAdmin,  deleteInquiry);
+inquiresRouter.delete("/:id",isLoggedin, isAdmin,  deleteInquiry);
 
 export default inquiresRouter;
