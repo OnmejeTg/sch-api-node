f0828aee9cbf4eb8d203074507cb2c8c5e3d3104
Onmeje TG <tonmeje@gmail.com>
2024-07-07 22:00:04 +0100

    corrected permission

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -10,19 +10,29 @@ import {
   uploadPicture,
   createStudentAuthUser,
 } from "../controllers/students/studentController.js";
-import { isAdmin, isLoggedin, isStudent } from "../middleware/auth.js";
+import {
+  isAdmin,
+  isTeacherOrAdmin,
+  isLoggedin,
+  isStudent,
+} from "../middleware/auth.js";
 import { memoryupload, upload } from "../utils/multer.js";
 import { validateStudentRegistration } from "../validators/studentValidator.js";
-
 
 const studentRouter = express.Router();
 
 studentRouter.post("/login", login);
 studentRouter.post("/logout", logout);
-studentRouter.post("/create",validateStudentRegistration, registerStudent);
-studentRouter.get("/all", isAdmin, getStudents);
-studentRouter.get("/get/:id", isAdmin, getStudent);
-studentRouter.put("/update", memoryupload.single("file"), isLoggedin, isStudent, updateStudent);
+studentRouter.post("/create", validateStudentRegistration, registerStudent);
+studentRouter.get("/all", isTeacherOrAdmin, getStudents);
+studentRouter.get("/get/:id", isTeacherOrAdmin, getStudent);
+studentRouter.put(
+  "/update",
+  memoryupload.single("file"),
+  isLoggedin,
+  isStudent,
+  updateStudent
+);
 studentRouter.delete("/delete/:id", isAdmin, deleteStudent);
 studentRouter.post("/upload-pics/", uploadPicture);
 studentRouter.get("/createAuthUser/:id", createStudentAuthUser);
